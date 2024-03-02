a13323930efb6043a89954a98015db53f87b77a2
Twana Gul <twanagul@twanas-macbook-pro-1.home>
2024-03-02 22:25:42 +0100

    fixed editable component in preview mode

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -57,7 +57,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
     <div className="pb-40">
       <Cover preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-        <Toolbar initialData={document} />
+        <Toolbar preview initialData={document} />
         <Editor
           editable={false}
           onChange={onChange}
